c826bd7cb26c8d9143797de3b7ada219b8cf6df8
Dmitry Turovtsov <web.best.master@gmail.com>
2016-11-12 18:21:33 +0300

    add main initializations

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,19 @@
 (function (win) {
 
+    "use strict";
 
+    function main() {
 
+        var imageOverlay = new ImageOverlay();
+
+        imageOverlay
+            .initializeImage('image/death-stars.jpg')
+            .then(function () {
+                document.body.appendChild(imageOverlay.getCanvas());
+            });
+
+    }
+
+    win.addEventListener('load', main, false);
 
 }(window));
--- www/js/our-plugin/plugin.js
+++ www/js/our-plugin/plugin.js
@@ -1,6 +1,119 @@
 (function (win) {
 
+    "use strict";
+
+    win.ImageOverlay = ImageOverlay;
+
+    function ImageOverlay() {
+
+        var imageOverlay = this;
+
+        // create renderer
+        imageOverlay._setContainer(new PIXI.Container());
+        imageOverlay._setRenderer(PIXI.autoDetectRenderer(128, 128));
+
+        imageOverlay._initializeTicker();
+
+    }
+
+    ImageOverlay.prototype.initializeImage = function (pathToImage) {
+
+        var imageOverlay = this;
+        var stage = imageOverlay.getContainer();
+        var renderer = imageOverlay.getRenderer();
+
+        return new Promise(function (resolve, reject) {
+
+            var sprite = new PIXI.Sprite.fromImage(pathToImage);
+
+            sprite
+                .texture.baseTexture
+                .on('loaded', function () {
+                    renderer.resize(sprite.width, sprite.height);
+                    stage.addChild(sprite);
+                    resolve();
+                })
+                .on('error', reject);
+
+        });
+
+    };
+
+    //////////////////////////////////////////////////
+    // Private methods and properties
+    //////////////////////////////////////////////////
+
+    ImageOverlay.prototype._update = function () {
+        // console.log('_update');
+        this._renderer.render(this._container);
+    };
+
+    ImageOverlay.prototype._container = null;
+    ImageOverlay.prototype._renderer = null;
+    ImageOverlay.prototype._ticker = null;
+
+    //////////////////////////////////////////////////
+    // Ticker
+    //////////////////////////////////////////////////
+
+    ImageOverlay.prototype._initializeTicker = function () {
+
+        var ticker = new PIXI.ticker.Ticker();
+        var imageOverlay = this;
+
+        ticker.add(imageOverlay._update, imageOverlay);
+
+        ticker.start();
+
+        imageOverlay._setTicker(ticker);
+
+    };
+
+    ImageOverlay.prototype._destroyTicker = function () {
+
+        var imageOverlay = this;
+
+        var ticker = imageOverlay.getTicker();
+
+        ticker.remove(imageOverlay._update, imageOverlay);
+        ticker.stop();
+
+    };
+
+    //////////////////////////////////////////////////
+    // Getters / Setters
+    //////////////////////////////////////////////////
+
+    // only GET
+    ImageOverlay.prototype.getCanvas = function () {
+        return this._renderer.view;
+    };
 
 
+    ImageOverlay.prototype.getTicker = function () {
+        return this._ticker;
+    };
+
+    ImageOverlay.prototype._setTicker = function (ticker) {
+        return this._ticker = ticker;
+    };
+
+
+    ImageOverlay.prototype.getRenderer = function () {
+        return this._renderer;
+    };
+
+    ImageOverlay.prototype._setRenderer = function (renderer) {
+        return this._renderer = renderer;
+    };
+
+
+    ImageOverlay.prototype.getContainer = function () {
+        return this._container;
+    };
+
+    ImageOverlay.prototype._setContainer = function (container) {
+        return this._container = container;
+    };
 
 }(window));
